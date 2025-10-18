709d0f6e6bb773ca9c9512b45c9e98d976382419
ahmedgfouad <ahmedgfouad2020@gmail.com>
2025-10-18 17:59:28 +0300

    finish details feature

--- lib/features/details/presentation/presentation/widgets/image_widget.dart
+++ lib/features/details/presentation/presentation/widgets/image_widget.dart
@@ -1,17 +1,15 @@
-
-import 'package:cat_api/core/constants/images.dart';
 import 'package:cat_api/core/theme/colors.dart';
 import 'package:cat_api/core/utils/media_query_values.dart';
 import 'package:flutter/material.dart';
 
 class ImageWidget extends StatelessWidget {
-  const ImageWidget({
-    super.key,
-  });
+  const ImageWidget({super.key, required this.image});
+  final String image;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(10),
       height: context.height / 3,
       width: context.width,
       decoration: BoxDecoration(
@@ -21,7 +19,12 @@ class ImageWidget extends StatelessWidget {
           bottomRight: Radius.circular(20),
         ),
       ),
-      child: Center(child: Image.asset(AppImages.dog2)),
+      child: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(image),
+        ),
+      ),
     );
   }
 }
--- lib/features/details/presentation/presentation/widgets/name_and_price_widget.dart
+++ lib/features/details/presentation/presentation/widgets/name_and_price_widget.dart
@@ -1,12 +1,11 @@
-
 import 'package:cat_api/core/theme/colors.dart';
 import 'package:cat_api/core/theme/styles.dart';
 import 'package:flutter/material.dart';
 
 class NameAndPriceWidget extends StatelessWidget {
-  const NameAndPriceWidget({
-    super.key,
-  });
+  const NameAndPriceWidget({super.key, required this.name, required this.location});
+
+  final String name ,location; 
 
   @override
   Widget build(BuildContext context) {
@@ -16,13 +15,13 @@ class NameAndPriceWidget extends StatelessWidget {
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Tom", style: AppStyles.textStyle22(context)),
+            Text(name, style: AppStyles.textStyle22(context)),
             SizedBox(height: 5),
             Row(
               children: [
                 Icon(Icons.location_on, color: Colors.red),
                 SizedBox(width: 5),
-                Text("2.7 km away"),
+                Text(location),
               ],
             ),
           ],
